a5e5ef0ccf2638ff6168d96f458ba7ae23d2313f
Rudolf Markulin <rudolf.markulin@protonmail.com>
2016-04-25 14:54:27 +0200

    return whole structure, prepare for print color schemes

--- src/main/scala/pretty.scala
+++ src/main/scala/pretty.scala
@@ -4,20 +4,24 @@ import java.time.LocalDate
 
 import shapeless._
 
+trait EQQ
+case class Different(value:String) extends EQQ
+case class Same(value:String) extends EQQ
+
 case class Address(street:String, city:String)
 case class User(name:String, lastName:String, birthDate:LocalDate, address:Address)
 
 object CaseClassDiff  extends App{
   private implicit def fooIso = Generic[User]
 
-  private def diff[H1 <: HList, H2 <: HList](lst1: H1, lst2: H2)(implicit e: H1 =:= H2): List[String] = (lst1, lst2) match {
-    case (HNil, HNil)                 => List()
-    case (h1::t1, h2::t2) if h1 != h2 => s"$h1 -> $h2" :: diff(t1, t2)
-    case (h1::t1, h2::t2)             => diff(t1, t2)
+  private def diff[H1 <: HList, H2 <: HList](lst1: H1, lst2: H2)(implicit e: H1 =:= H2): List[EQQ] = (lst1, lst2) match {
+    case (HNil, HNil)                 => Nil
+    case (h1::t1, h2::t2) if h1 != h2 => Different(s"$h1") :: diff(t1, t2)
+    case (h1::t1, h2::t2)             => Same(s"$h1") :: diff(t1, t2)
     case _                            => throw new RuntimeException("something went very wrong")
   }
 
-  def difference(value1:User, value2:User): List[String] = {
+  def difference(value1:User, value2:User): List[EQQ] = {
     val v1 = fooIso.to(value1)
     val v2 = fooIso.to(value2)
     diff(v1, v2)
